813d32c2421e36ce0d38f098f3fd626e408dc0d8
Gesselle Lopoz <gesselle03310@gmail.com>
2022-10-24 10:56:10 +0800

    First Commit

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,21 @@
+@extends('base')
+
+@section('content')
+<div class="container">
+    <div class="card mt-4">
+        {{-- style="; --}}
+    <div class=" items-center justify-center text-center">
+        <div class="card-body bg-dark">
+            <h1 class="text-white">Welcome to Midterm CheapTalk!</h1>
+            <!-- @if (!Auth::check())
+            <a href="{{url('/register')}}"
+                class="btn btn-primary">
+            Get Started
+            </a>
+            @endif -->
+
+        </div>
+    </div>
+</div>
+</div>
+@endsection
